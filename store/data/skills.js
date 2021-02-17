40b9a7eb1c20ea5cda852578b95c0bd764b2b546
agusramufhit <agusramufhit50@gmail.com>
2021-02-18 01:04:10 +0700

    fixed v2

--- store/data/skills.js
+++ store/data/skills.js
@@ -2,16 +2,11 @@ export default [
     {
     icon: 'devicon-photoshop-plain colored',
     name: 'Photoshop',
-    years: '3 months',
+    years: '2 years',
     },
     {
     icon_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/33/Figma-logo.svg/160px-Figma-logo.svg.png',
     name: 'Figma',
-    years: '3 months',
-    },
-    {
-    icon: "devicon-go-plain colored",
-    name: 'Golang',
     years: '6 months',
     },
     {
@@ -22,12 +17,12 @@ export default [
     {
     icon: "devicon-php-plain colored",
     name: 'PHP',
-    years: '3 years',
+    years: '2 years',
     },
     {
     icon: "devicon-laravel-plain colored",
     name: 'Laravel',
-    years: '3 years',
+    years: '1 years',
     },
     {
     icon: "devicon-sass-plain colored",
@@ -37,56 +32,26 @@ export default [
     {
     icon: "devicon-vuejs-plain colored",
     name: 'VueJS',
-    years: '2 years',
+    years: '1 years',
     },
     {
     icon: "devicon-react-plain colored",
     name: 'React',
-    years: '3 months',
-    },
-    {
-    icon: "devicon-jquery-plain colored",
-    name: 'jQUery',
-    years: '3 years',
-    },
-    {
-    icon: "devicon-express-original",
-    name: 'ExpressJS',
-    years: '6 months',
-    },
-    {
-    icon: "devicon-python-plain colored",
-    name: 'Python',
     years: '6 months',
     },
     {
     icon: "devicon-nodejs-plain colored",
     name: 'NodeJS',
-    years: '6 months',
+    years: '3 months',
     },
     {
     icon: "devicon-wordpress-plain colored",
     name: 'Wordpress',
-    years: '1 year',
-    },
-    {
-    icon: "devicon-redis-plain colored",
-    name: 'Redis',
-    years: '8 months',
+    years: '2 year',
     },
     {
     icon: "devicon-mysql-plain colored",
     name: 'MySQL',
     years: '3 years',
-    },
-    {
-    icon: "devicon-postgresql-plain colored",
-    name: 'PostgreSQL',
-    years: '2 years',
-    },
-    { 
-    icon: "devicon-mongodb-plain colored",
-    name: 'MongoDB',
-    years: '1 year',
     }
 ]
